8c1ecd739d65ef16c655b7b543e1ddaac7031e50
Mandy Pytleski <pytleskia5362@my.uwstout.edu>
2026-02-01 12:48:05 -0600

    END OF PROJECT and switched back to html

--- includes/cta.php
+++
@@ -1,3 +0,0 @@
-<h2>Ready to Elevate Your Online Presence?</h2>
-                <p>Contact me today to discuss how I can help you achieve your digital goals!</p>
-                <a class="cta-button" href="../contact.html">Get in Touch</a>
